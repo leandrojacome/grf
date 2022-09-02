c848fcddd512d3ee50760512638c32ad70d92f32
Leonardo Ferreira Alves <leonardoalves.eireli@poupex.com.br>
2022-09-02 19:10:35 -0300

    EasyRandon init

---
+++ src/main/java/br/com/poupex/investimento/recursosfinanceiros/infrastructure/EasyRandomConfig.java
@@ -0,0 +1,22 @@
+package br.com.poupex.investimento.recursosfinanceiros.infrastructure;
+
+import br.com.poupex.investimento.recursosfinanceiros.entity.model.InstituicaoFinanceiraOutput;
+import lombok.RequiredArgsConstructor;
+import org.jeasy.random.EasyRandom;
+import org.jeasy.random.EasyRandomParameters;
+import org.jeasy.random.FieldPredicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class EasyRandomConfig {
+
+  @Bean
+  public EasyRandom generator() {
+    EasyRandomParameters parameters = new EasyRandomParameters();
+    parameters.excludeField(FieldPredicates.named("grupo").and(FieldPredicates.inClass(InstituicaoFinanceiraOutput.class)));
+    return new EasyRandom(parameters);
+  }
+
+}
