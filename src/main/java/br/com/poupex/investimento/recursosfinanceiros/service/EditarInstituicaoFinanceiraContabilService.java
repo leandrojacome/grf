8cc64ce2d8059db7c93767daeeaf850642573a6b
Leonardo Ferreira Alves <leonardoalves.eireli@poupex.com.br>
2022-10-05 15:24:33 -0300

    Lazy config

--- src/main/java/br/com/poupex/investimento/recursosfinanceiros/service/EditarInstituicaoFinanceiraContabilService.java
+++ src/main/java/br/com/poupex/investimento/recursosfinanceiros/service/EditarInstituicaoFinanceiraContabilService.java
@@ -4,12 +4,14 @@ import br.com.poupex.investimento.recursosfinanceiros.entity.data.InstituicaoFin
 import br.com.poupex.investimento.recursosfinanceiros.entity.model.ContabilInputOutput;
 import br.com.poupex.investimento.recursosfinanceiros.entity.model.EnderecoInputOutput;
 import br.com.poupex.investimento.recursosfinanceiros.entity.model.ResponseModel;
+import br.com.poupex.investimento.recursosfinanceiros.exception.RecursoNaoEncontradoException;
 import br.com.poupex.investimento.recursosfinanceiros.repository.InstituicaoFinanceiraContabilRepository;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.BeanUtils;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -20,23 +22,29 @@ public class EditarInstituicaoFinanceiraContabilService {
   private final ModelMapper mapper;
   private final ObterInstituicaoFinanceiraContabilService obterInstituicaoFinanceiraContabilService;
   private final InstituicaoFinanceiraContabilRepository instituicaoFinanceiraContabilRepository;
+  @Lazy
+  private final CadastrarInstituicaoFinanceiraContabilService cadastrarInstituicaoFinanceiraContabilService;
 
   public ResponseModel execute(final String id, final ContabilInputOutput input) {
-    val contabil = obterInstituicaoFinanceiraContabilService.id(id);
-    BeanUtils.copyProperties(
-      mapper.map(input, InstituicaoFinanceiraContabil.class), contabil,
-      "id", "instituicaoFinanceira", "cadastro", "atualizacao"
-    );
-    return new ResponseModel(
-      LocalDateTime.now(),
-      HttpStatus.OK.value(),
-      "Alteração realizada com sucesso",
-      String.format("Os Dados Contábeis da Instituição %s foram alterados com sucesso", id),
-      "Dados Contábeis da Instituição alterados com sucesso",
-      null,
-      mapper.map(instituicaoFinanceiraContabilRepository.save(contabil), EnderecoInputOutput.class
-      )
-    );
+    try {
+      val contabil = obterInstituicaoFinanceiraContabilService.id(id);
+      BeanUtils.copyProperties(
+        mapper.map(input, InstituicaoFinanceiraContabil.class), contabil,
+        "id", "instituicaoFinanceira", "cadastro", "atualizacao"
+      );
+      return new ResponseModel(
+        LocalDateTime.now(),
+        HttpStatus.OK.value(),
+        "Alteração realizada com sucesso",
+        String.format("Os Dados Contábeis da Instituição %s foram alterados com sucesso", id),
+        "Dados Contábeis da Instituição alterados com sucesso",
+        null,
+        mapper.map(instituicaoFinanceiraContabilRepository.save(contabil), EnderecoInputOutput.class
+        )
+      );
+    } catch (final RecursoNaoEncontradoException e) {
+      return cadastrarInstituicaoFinanceiraContabilService.execute(id, input);
+    }
   }
 
 }
--- src/main/java/br/com/poupex/investimento/recursosfinanceiros/service/EditarInstituicaoFinanceiraRiscoService.java
+++ src/main/java/br/com/poupex/investimento/recursosfinanceiros/service/EditarInstituicaoFinanceiraRiscoService.java
@@ -3,12 +3,14 @@ package br.com.poupex.investimento.recursosfinanceiros.service;
 import br.com.poupex.investimento.recursosfinanceiros.entity.data.InstituicaoFinanceiraRisco;
 import br.com.poupex.investimento.recursosfinanceiros.entity.model.ResponseModel;
 import br.com.poupex.investimento.recursosfinanceiros.entity.model.RiscoInputOutput;
+import br.com.poupex.investimento.recursosfinanceiros.exception.RecursoNaoEncontradoException;
 import br.com.poupex.investimento.recursosfinanceiros.repository.InstituicaoFinanceiraRiscoRepository;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.BeanUtils;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -20,24 +22,30 @@ public class EditarInstituicaoFinanceiraRiscoService {
   private final ValidaInstituicaoFinanceiraRiscosOpcoesService validaInstituicaoFinanceiraRiscosOpcoesService;
   private final ObterInstituicaoFinanceiraRiscoService obterInstituicaoFinanceiraRiscoService;
   private final InstituicaoFinanceiraRiscoRepository instituicaoFinanceiraRiscoRepository;
+  @Lazy
+  private final CadastrarInstituicaoFinanceiraRiscoService cadastrarInstituicaoFinanceiraRiscoService;
 
   public ResponseModel execute(final String id, final RiscoInputOutput input) {
-    validaInstituicaoFinanceiraRiscosOpcoesService.execute(input);
-    val riscos = obterInstituicaoFinanceiraRiscoService.id(id);
-    BeanUtils.copyProperties(
-      mapper.map(input, InstituicaoFinanceiraRisco.class), riscos,
-      "id", "instituicaoFinanceira", "cadastro", "atualizacao"
-    );
-    return new ResponseModel(
-      LocalDateTime.now(),
-      HttpStatus.OK.value(),
-      "Alteração realizada com sucesso",
-      String.format("Os Riscos da Instituição %s foram alterados com sucesso", id),
-      "Riscos da Instituição alterados com sucesso",
-      null,
-      mapper.map(instituicaoFinanceiraRiscoRepository.save(riscos), RiscoInputOutput.class
-      )
-    );
+    try {
+      validaInstituicaoFinanceiraRiscosOpcoesService.execute(input);
+      val riscos = obterInstituicaoFinanceiraRiscoService.id(id);
+      BeanUtils.copyProperties(
+        mapper.map(input, InstituicaoFinanceiraRisco.class), riscos,
+        "id", "instituicaoFinanceira", "cadastro", "atualizacao"
+      );
+      return new ResponseModel(
+        LocalDateTime.now(),
+        HttpStatus.OK.value(),
+        "Alteração realizada com sucesso",
+        String.format("Os Riscos da Instituição %s foram alterados com sucesso", id),
+        "Riscos da Instituição alterados com sucesso",
+        null,
+        mapper.map(instituicaoFinanceiraRiscoRepository.save(riscos), RiscoInputOutput.class
+        )
+      );
+    } catch (final RecursoNaoEncontradoException e) {
+      return cadastrarInstituicaoFinanceiraRiscoService.execute(id, input);
+    }
   }
 
 }
