2a43f5d496f958d6e8d2b3f7f720bf7016a1a9eb
EMERSON Esteves Martins <emersonmartins.eireli@poupex.com.br>
2022-10-28 09:24:31 -0300

    Ajustes do Mark para funcionar o Page no GIFxGRF

--- src/main/java/br/com/poupex/investimento/recursosfinanceiros/infrastructure/client/GestaoInstrumentosFinanceirosApiClient.java
+++ src/main/java/br/com/poupex/investimento/recursosfinanceiros/infrastructure/client/GestaoInstrumentosFinanceirosApiClient.java
@@ -1,17 +1,18 @@
 package br.com.poupex.investimento.recursosfinanceiros.infrastructure.client;
 
-import br.com.poupex.investimento.recursosfinanceiros.domain.model.gif.InstrumentoFinanceiroOutput;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.poupex.investimento.recursosfinanceiros.domain.model.gif.InstrumentoFinanceiroOutput;
 
 
 @FeignClient(name = "gestaoInstrumentosFinanceirosApiClient",
         url = "${poupex.api.terceiros.gestao-instrumentos-api-url}")
 public interface GestaoInstrumentosFinanceirosApiClient {
 
-    @GetMapping("/v1/05e2a7bb-e6a3-47d5-883e-1c3162bf2279")
-    PageImpl<InstrumentoFinanceiroOutput> getInstrumentosFinanceiros(Pageable pageable);
+    @GetMapping("/instrumento-financeiro/listar/ativos/paginado/index.json")
+    Page<InstrumentoFinanceiroOutput> getInstrumentosFinanceiros(Pageable pageable);
 
 }
--- src/main/java/br/com/poupex/investimento/recursosfinanceiros/infrastructure/mapper/MapperConfig.java
+++ src/main/java/br/com/poupex/investimento/recursosfinanceiros/infrastructure/mapper/MapperConfig.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.val;
 import org.modelmapper.ModelMapper;
+import org.springframework.cloud.openfeign.support.PageJacksonModule;
+import org.springframework.cloud.openfeign.support.SortJacksonModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +17,8 @@ public class MapperConfig {
   public ObjectMapper objectMapper() {
     val mapper = new ObjectMapper();
     mapper.registerModule(new JavaTimeModule());
+    mapper.registerModule(new PageJacksonModule());
+    mapper.registerModule(new SortJacksonModule());
     mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     mapper.findAndRegisterModules();
     return mapper;
