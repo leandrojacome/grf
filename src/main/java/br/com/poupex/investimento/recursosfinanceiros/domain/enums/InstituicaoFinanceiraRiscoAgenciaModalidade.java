d05c3b35fabedff1380158c9c2bf78bc99a05ec4
Leonardo Ferreira Alves <leonardoalves.eireli@poupex.com.br>
2022-10-31 15:13:43 -0300

    Ajuste e criação endpoint agencias

--- src/main/java/br/com/poupex/investimento/recursosfinanceiros/domain/enums/InstituicaoFinanceiraRiscoAgenciaModalidade.java
+++ src/main/java/br/com/poupex/investimento/recursosfinanceiros/domain/enums/InstituicaoFinanceiraRiscoAgenciaModalidade.java
@@ -1,5 +1,16 @@
 package br.com.poupex.investimento.recursosfinanceiros.domain.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum InstituicaoFinanceiraRiscoAgenciaModalidade {
-  CLASSIFICACAO, MOODYS, S_P, FITCH, RISK_BANK, POUPEX
+  CLASSIFICACAO("Classificação"),
+  MOODYS("Moodys"),
+  S_P("S&P"), FITCH("FITCH"),
+  RISK_BANK("RiskBanking"),
+  POUPEX("Poupex");
+
+  private final String label;
 }
---
+++ src/main/java/br/com/poupex/investimento/recursosfinanceiros/service/RecuperarRiscoClassificacoesPorAgenciaModalidadeService.java
@@ -0,0 +1,26 @@
+package br.com.poupex.investimento.recursosfinanceiros.service;
+
+import br.com.poupex.investimento.recursosfinanceiros.domain.enums.InstituicaoFinanceiraRiscoAgenciaModalidade;
+import br.com.poupex.investimento.recursosfinanceiros.domain.enums.InstituicaoFinanceiraRiscoClassificacao;
+import br.com.poupex.investimento.recursosfinanceiros.domain.model.ChaveLabelDescricaoOutput;
+import br.com.poupex.investimento.recursosfinanceiros.domain.model.ResponseModel;
+import java.time.LocalDateTime;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class RecuperarRiscoClassificacoesPorAgenciaModalidadeService {
+
+  public ResponseModel execute(final InstituicaoFinanceiraRiscoAgenciaModalidade agenciaModalidade) {
+    return new ResponseModel(
+      LocalDateTime.now(),
+      HttpStatus.OK.value(),
+      null, null, null, null,
+      InstituicaoFinanceiraRiscoClassificacao.findByAgenciaModalidade(agenciaModalidade).stream()
+        .map(opcao -> new ChaveLabelDescricaoOutput(opcao.name(), opcao.getLabel(), opcao.getLabel())).toList()
+    );
+  }
+
+}
