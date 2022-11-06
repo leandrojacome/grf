718fd2bcd293a013c911355345ba7e7c5be75608
Hugo Nóbrega <hugo.nobreg@gmail.com>
2022-11-06 18:55:28 -0300

    GIF-291 <Ajuste para evitar repetição de parametro no swagger>

--- src/main/java/br/com/poupex/investimento/recursosfinanceiros/api/controller/InstrumentosFinanceirosController.java
+++ src/main/java/br/com/poupex/investimento/recursosfinanceiros/api/controller/InstrumentosFinanceirosController.java
@@ -21,10 +21,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("instrumentos-financeiros")
@@ -63,7 +60,9 @@ public class InstrumentosFinanceirosController {
 			}),
 	})
 	@GetMapping("{id}")
-	public ResponseEntity<ResponseModel> read(@Parameter(name = "id", description = "Identificador do Instrumento Financeiro", required = true) final int id) {
+	public ResponseEntity<ResponseModel> read(
+			@Parameter(name = "id", description = "Identificador do Instrumento Financeiro")
+			@PathVariable final int id) {
 		return ResponseEntity.ok(obterInstrumentoFinancerioService.execute(id));
 	}
 
