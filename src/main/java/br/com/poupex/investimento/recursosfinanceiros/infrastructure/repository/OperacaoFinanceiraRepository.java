a19a88281ca5ef2c79cd768035337289ef7820a6
EMERSON Esteves Martins <emersonmartins.eireli@poupex.com.br>
2022-11-14 09:31:01 -0300

    GIF-319 listagem da operaçao financeira

--- src/main/java/br/com/poupex/investimento/recursosfinanceiros/infrastructure/repository/OperacaoFinanceiraRepository.java
+++ src/main/java/br/com/poupex/investimento/recursosfinanceiros/infrastructure/repository/OperacaoFinanceiraRepository.java
@@ -3,8 +3,8 @@ package br.com.poupex.investimento.recursosfinanceiros.infrastructure.repository
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import br.com.poupex.investimento.recursosfinanceiros.domain.entity.InstituicaoFinanceira;
+import br.com.poupex.investimento.recursosfinanceiros.domain.entity.OperacaoFinanceira;
 
-public interface OperacaoFinanceiraRepository extends JpaRepository<InstituicaoFinanceira, String>, JpaSpecificationExecutor<InstituicaoFinanceira> {
+public interface OperacaoFinanceiraRepository extends JpaRepository<OperacaoFinanceira, String>, JpaSpecificationExecutor<OperacaoFinanceira> {
 
 }
