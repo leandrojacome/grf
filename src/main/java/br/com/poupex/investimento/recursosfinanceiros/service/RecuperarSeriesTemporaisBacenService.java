a84b221862a2a6b8698ed6280ffcb2e487713f1e
02997774136 <02997774136>
2022-11-21 16:06:27 -0300

    adicionado a regra da data piso do ano de 2000

--- src/main/java/br/com/poupex/investimento/recursosfinanceiros/service/RecuperarSeriesTemporaisBacenService.java
+++ src/main/java/br/com/poupex/investimento/recursosfinanceiros/service/RecuperarSeriesTemporaisBacenService.java
@@ -19,6 +19,7 @@ import java.util.Objects;
 public class RecuperarSeriesTemporaisBacenService {
 
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("d/MM/yyyy");
+    private static final LocalDate DATA_PISO_SERIE = LocalDate.of(2000, 01, 01);
     private final SeriesTemporaisBacenApiClient seriesTemporaisBacenApiClient;
 
     public List<SeriesTemporaisOutput> execute(final Long codigo, LocalDate dataInicio, LocalDate dataFinal) {
@@ -31,6 +32,10 @@ public class RecuperarSeriesTemporaisBacenService {
     private SeriesTemporaisInput validaPeriodoPesquisa(final LocalDate dataInicio, final LocalDate dataFinal) throws NegocioException {
         if(Objects.nonNull(dataInicio) && dataInicio.isAfter(dataFinal)){
                 throw new NegocioException("Serie ", "Data Início maior que data fim");
+        }
+
+        if( dataInicio.isBefore(DATA_PISO_SERIE)) {
+            throw new NegocioException("Série temporal de indicadores ", "Data Inicio deve ser maior que 01/01/2000");
         }
 
         final var dataInicioPesquisa = Objects.requireNonNullElse(dataInicio, LocalDate.now());
