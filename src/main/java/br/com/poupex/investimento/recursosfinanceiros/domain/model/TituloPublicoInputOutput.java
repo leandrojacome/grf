5176a19f9d3107c34f7beae9cd35d27dc9f231e2
EMERSON Esteves Martins <emersonmartins.eireli@poupex.com.br>
2022-12-01 14:42:43 -0300

    Listar titulos publicos

---
+++ src/main/java/br/com/poupex/investimento/recursosfinanceiros/domain/model/TituloPublicoInputOutput.java
@@ -0,0 +1,22 @@
+package br.com.poupex.investimento.recursosfinanceiros.domain.model;
+
+import java.time.LocalDateTime;
+
+import br.com.poupex.investimento.recursosfinanceiros.domain.enums.TipoMercado;
+import lombok.Data;
+
+@Data
+public class TituloPublicoInputOutput {
+	
+	private String nome; // gif
+	private Long codFormaMensuracao; // gif
+	private Boolean ativoFinanceiro; // gif
+	private String siglaGif;
+    private String isin;
+    private TipoMercado tipo;
+    private LocalDateTime dataEmissao;
+    private LocalDateTime dataVencimento;
+    private Long codiogSelic;
+    private Boolean cupom;
+
+}
---
+++ src/main/java/br/com/poupex/investimento/recursosfinanceiros/service/ObterListaTituloPublicoService.java
@@ -0,0 +1,45 @@
+package br.com.poupex.investimento.recursosfinanceiros.service;
+
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import br.com.poupex.investimento.recursosfinanceiros.domain.entity.TituloPublico;
+import br.com.poupex.investimento.recursosfinanceiros.domain.model.FilterTituloPublicoInput;
+import br.com.poupex.investimento.recursosfinanceiros.domain.model.PageOutput;
+import br.com.poupex.investimento.recursosfinanceiros.domain.model.ResponseModel;
+import br.com.poupex.investimento.recursosfinanceiros.domain.model.TituloPublicoInputOutput;
+import br.com.poupex.investimento.recursosfinanceiros.infrastructure.repository.TituloPublicoRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+
+@Service
+@RequiredArgsConstructor
+public class ObterListaTituloPublicoService {
+
+	private final TituloPublicoRepository tituloPublicoRepository;
+
+	private final ModelMapper mapper;
+
+	public ResponseModel execute(FilterTituloPublicoInput filter, Pageable pageable) {
+		
+		val tituloPublico = mapper.map(filter, TituloPublico.class);
+		val resultado = tituloPublicoRepository.findAll(Example.of(tituloPublico), pageable);
+		val mensagem = resultado.getTotalElements() == 0 ? "Nenhum registro encontrado" : null;
+		
+		val page = new PageImpl<>(resultado.getContent().stream()
+				.map(r -> mapper.map(r, TituloPublicoInputOutput.class)).collect(Collectors.toList()), pageable,
+				resultado.getTotalElements());
+		
+		return new ResponseModel(LocalDateTime.now(), HttpStatus.OK.value(), null, null, mensagem, null,
+				mapper.map(page, PageOutput.class));
+
+	}
+
+}
