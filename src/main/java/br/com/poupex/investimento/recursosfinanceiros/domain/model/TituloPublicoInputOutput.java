d8e8adf968df141dece6cf7ab0b5d4eca306fa8a
EMERSON Esteves Martins <emersonmartins.eireli@poupex.com.br>
2022-12-05 11:24:52 -0300

    GIF-386 Inclusao do id no dto

--- src/main/java/br/com/poupex/investimento/recursosfinanceiros/domain/model/TituloPublicoInputOutput.java
+++ src/main/java/br/com/poupex/investimento/recursosfinanceiros/domain/model/TituloPublicoInputOutput.java
@@ -2,12 +2,19 @@ package br.com.poupex.investimento.recursosfinanceiros.domain.model;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
 import br.com.poupex.investimento.recursosfinanceiros.domain.enums.TipoMercado;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class TituloPublicoInputOutput {
 	
+	@Schema(accessMode = Schema.AccessMode.READ_ONLY)
+	@JsonProperty(access = Access.READ_ONLY)
+	private String id;
 	private String nome; // gif
 	private Long codFormaMensuracao; // gif
 	private Boolean ativoFinanceiro; // gif
