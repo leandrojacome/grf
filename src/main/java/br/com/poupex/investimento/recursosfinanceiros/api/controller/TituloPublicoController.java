2e0262b4b4edb56791709e0dacc79c33064421ba
EMERSON Esteves Martins <emersonmartins.eireli@poupex.com.br>
2022-12-05 11:50:28 -0300

    GIF-272 Correcao do RequestBody

--- src/main/java/br/com/poupex/investimento/recursosfinanceiros/api/controller/TituloPublicoController.java
+++ src/main/java/br/com/poupex/investimento/recursosfinanceiros/api/controller/TituloPublicoController.java
@@ -3,8 +3,8 @@ package br.com.poupex.investimento.recursosfinanceiros.api.controller;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.poupex.investimento.recursosfinanceiros.api.common.OpenApiPaginacao;
