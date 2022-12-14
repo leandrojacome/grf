86a707b182340f127bbb195603d45a9bb369e163
EMERSON Esteves Martins <emersonmartins.eireli@poupex.com.br>
2022-12-14 15:44:19 -0300

    Retirada do MantidoVencimento

--- src/main/java/br/com/poupex/investimento/recursosfinanceiros/service/CadastrarFundosInvestimentosService.java
+++ src/main/java/br/com/poupex/investimento/recursosfinanceiros/service/CadastrarFundosInvestimentosService.java
@@ -35,7 +35,6 @@ public class CadastrarFundosInvestimentosService {
         inputGif.setCodInstituicao(getCodInstituicao());
         inputGif.setSemPassivos(false);
         inputGif.setSemTestesSppj(false);
-        inputGif.setMantidoVencimento(true);
         inputGif.setSigla("QUALQUER");
         inputGif.setCodModeloNegocio(getCodModeloNegocio("M02"));
 
