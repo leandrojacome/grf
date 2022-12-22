7483886db91625167cb25dde02837db7ad6d5499
EMERSON Esteves Martins <emersonmartins.eireli@poupex.com.br>
2022-12-22 16:56:34 -0300

    GIF-436 Ajuste do codigo operacao renda fixa

--- src/main/java/br/com/poupex/investimento/recursosfinanceiros/infrastructure/repository/OperacaoFinanceiraRepository.java
+++ src/main/java/br/com/poupex/investimento/recursosfinanceiros/infrastructure/repository/OperacaoFinanceiraRepository.java
@@ -2,6 +2,7 @@ package br.com.poupex.investimento.recursosfinanceiros.infrastructure.repository
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.poupex.investimento.recursosfinanceiros.domain.entity.OperacaoFinanceira;
 
@@ -9,4 +10,6 @@ public interface OperacaoFinanceiraRepository extends JpaRepository<OperacaoFina
 
 	Boolean existsByInstrumentoFinanceiroGifCodigo(Long codigo);
 
+	@Query(value = "SELECT GESTAO_RECURSOS_FINANCEIROS.NUMERO_OPERACAO_SEQ.nextval FROM dual", nativeQuery = true)
+	Long getNextNumeroOperacaoSeq();
 }
