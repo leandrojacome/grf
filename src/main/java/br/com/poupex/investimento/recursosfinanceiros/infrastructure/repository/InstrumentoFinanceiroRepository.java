d6f322b5ce7058fd48b6cd79a61f5e45a0068e68
EMERSON Esteves Martins <emersonmartins.eireli@poupex.com.br>
2023-01-10 12:24:09 -0300

    GIF-469 suporte para fundo de investimento

--- src/main/java/br/com/poupex/investimento/recursosfinanceiros/infrastructure/repository/InstrumentoFinanceiroRepository.java
+++ src/main/java/br/com/poupex/investimento/recursosfinanceiros/infrastructure/repository/InstrumentoFinanceiroRepository.java
@@ -1,5 +1,7 @@
 package br.com.poupex.investimento.recursosfinanceiros.infrastructure.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,7 +16,7 @@ public interface InstrumentoFinanceiroRepository extends JpaRepository<Instrumen
 	
 	@Query("SELECT if "
 		+ "FROM InstrumentoFinanceiro if "
-		+ "WHERE type(if) in (TituloPublico, TituloPrivado)"
+		+ "WHERE type(if) in (:tipoInstrumentos)"
 			)
-	Page<InstrumentoFinanceiro> findTituloPublicoPrivado(Pageable pageable);
+	Page<InstrumentoFinanceiro> findBySubtype(List<Class<?>> tipoInstrumentos, Pageable pageable);
 }
