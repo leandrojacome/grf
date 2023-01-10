586f50d393c713f8cc7329a7f03edb3ce8e9f51d
EMERSON Esteves Martins <emersonmartins.eireli@poupex.com.br>
2023-01-10 16:17:06 -0300

    Ajuste do filtro de instrumento financeiro

--- src/main/java/br/com/poupex/investimento/recursosfinanceiros/domain/model/FilterInstrumentoFinanceiroInput.java
+++ src/main/java/br/com/poupex/investimento/recursosfinanceiros/domain/model/FilterInstrumentoFinanceiroInput.java
@@ -7,5 +7,5 @@ import lombok.Data;
 
 @Data
 public class FilterInstrumentoFinanceiroInput {
-	private List<TipoInstrumentoFinanceiro> siglaTipoInstrumentoFinanceiro;
+	private List<TipoInstrumentoFinanceiro> tipoInstrumentoFinanceiro;
 }
