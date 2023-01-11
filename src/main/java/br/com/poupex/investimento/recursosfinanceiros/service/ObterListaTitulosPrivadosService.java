ac5ea536bbc3f122755de527b9908d66c66543dd
Hugo Nóbrega <hugonobrega.eireli@poupex.com.br>
2023-01-10 21:59:32 -0300

    GIF-465 Ajustando o serviço de listar de Titulo Privado

--- src/main/java/br/com/poupex/investimento/recursosfinanceiros/service/ObterListaTitulosPrivadosService.java
+++ src/main/java/br/com/poupex/investimento/recursosfinanceiros/service/ObterListaTitulosPrivadosService.java
@@ -1,23 +1,24 @@
 package br.com.poupex.investimento.recursosfinanceiros.service;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
+import br.com.poupex.investimento.recursosfinanceiros.domain.entity.TituloPrivado;
+import br.com.poupex.investimento.recursosfinanceiros.domain.model.FilterTituloPrivadoInput;
+import br.com.poupex.investimento.recursosfinanceiros.domain.model.ResponseModel;
+import br.com.poupex.investimento.recursosfinanceiros.domain.model.TituloPrivadoInputOutput;
+import br.com.poupex.investimento.recursosfinanceiros.domain.model.gif.InstrumentoFinanceiroGifInputOutput;
+import br.com.poupex.investimento.recursosfinanceiros.infrastructure.client.GestaoInstrumentosFinanceirosApiClient;
+import br.com.poupex.investimento.recursosfinanceiros.infrastructure.repository.TituloPrivadoRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import br.com.poupex.investimento.recursosfinanceiros.domain.model.FilterTituloPrivadoInput;
-import br.com.poupex.investimento.recursosfinanceiros.domain.model.ResponseModel;
-import br.com.poupex.investimento.recursosfinanceiros.domain.model.TituloPrivadoInputOutput;
-import br.com.poupex.investimento.recursosfinanceiros.infrastructure.client.GestaoInstrumentosFinanceirosApiClient;
-import br.com.poupex.investimento.recursosfinanceiros.infrastructure.repository.TituloPrivadoRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.val;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -32,34 +33,46 @@ public class ObterListaTitulosPrivadosService {
 
     public ResponseModel execute(FilterTituloPrivadoInput filter, Pageable pageable) {
         Long codMensuracao = null;
-		List<Long> codsTipos = new ArrayList<>();
+        List<Long> codsTipos = new ArrayList<>();
 
         filter = (filter == null ? new FilterTituloPrivadoInput() : filter);
 
-		codsTipos.add(obterTipoInstrumentoFinanceiroService.getCodTituloPrivado());
+        codsTipos.add(obterTipoInstrumentoFinanceiroService.getCodTituloPrivado());
 
         if (filter.getFormaMensuracao() != null)
             codMensuracao = filter.getFormaMensuracao().getCodigo();
 
-		val resultado = gestaoInstrumentosFinanceirosApiClient.getInstrumentosFinanceiros(pageable, codsTipos, filter.getNome(), filter.getSigla(), codMensuracao);
 
-        val page = new PageImpl<>(resultado.getContent().stream()
-                .map(r -> mapper.map(r, TituloPrivadoInputOutput.class)).collect(Collectors.toList()), pageable,
-                resultado.getTotalElements());
+        var titulosPrivados = tituloPrivadoRepository.findAll();
 
-        page.getContent().stream().forEach(titulo -> {
-        	try {
-        		var tituloPrivado = tituloPrivadoRepository.findByCodigoGif(titulo.getCodigo());
-                titulo.setId(tituloPrivado.getId());
-            } catch (Exception ignore) {
-            }
-        });
+        val resultado = gestaoInstrumentosFinanceirosApiClient.getInstrumentosFinanceiros(pageable, codsTipos, filter.getNome(), filter.getSigla(), codMensuracao);
+
+        var titulosPrivadosDTO = resultado.getContent().stream()
+                .filter(r -> hasTituloPrivado(r, titulosPrivados))
+                .map(r -> mapper.map(r, TituloPrivadoInputOutput.class))
+                .collect(Collectors.toList());
+
+        titulosPrivadosDTO.forEach(p -> setId(p, titulosPrivados));
+
+        var page = new PageImpl<>(titulosPrivadosDTO, pageable, titulosPrivadosDTO.size());
 
         return new ResponseModel(
                 LocalDateTime.now(),
                 HttpStatus.OK.value(),
                 null, null, null, null,
                 page);
+    }
+
+    private boolean hasTituloPrivado(InstrumentoFinanceiroGifInputOutput r, List<TituloPrivado> titulosPrivados) {
+        return titulosPrivados.stream().anyMatch(tp -> r.getCodigo().equals(tp.getCodigoGif()));
+    }
+
+    private void setId(TituloPrivadoInputOutput p, List<TituloPrivado> tituloPrivados) {
+        tituloPrivados.forEach((tp) -> {
+            if (tp.getCodigoGif().equals(p.getCodigo())) {
+                p.setId(tp.getId());
+            }
+        });
     }
 
 }
