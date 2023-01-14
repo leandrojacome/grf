a820f367bacef6dea85ad1b4e6bc68976e70c012
Leonardo Ferreira Alves <leonardo.alves@implantainformatica.com.br>
2023-01-14 19:48:58 -0300

    Entidade Operação Compromissada

--- src/main/java/br/com/poupex/investimento/recursosfinanceiros/domain/entity/OperacaoRendaFixaCompromissada.java
+++ src/main/java/br/com/poupex/investimento/recursosfinanceiros/domain/entity/OperacaoRendaFixaCompromissada.java
@@ -1,7 +1,11 @@
 package br.com.poupex.investimento.recursosfinanceiros.domain.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import br.com.poupex.investimento.recursosfinanceiros.domain.enums.Empresa;
+import br.com.poupex.investimento.recursosfinanceiros.domain.enums.FormaMensuracaoEnum;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import javax.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,4 +16,62 @@ import lombok.Setter;
 @Entity
 @Table(name = "OPERACAO_RENDA_FIXA_COMPROMISSADA", schema = "GESTAO_RECURSOS_FINANCEIROS")
 public class OperacaoRendaFixaCompromissada extends AbstractEntidadeBase {
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "EMPRESA", nullable = false)
+  private Empresa empresa;
+
+  @ManyToOne
+  @JoinColumn(name = "CONTRAPARTE_INSTITUICAO_FINANCEIRA", nullable = false)
+  private InstituicaoFinanceira contraparteInstituicaoFinanceira;
+
+  @Column(name = "CONTRAPARTE_CONTA_SELIC", nullable = false)
+  private Integer contraparteContaSelic;
+
+  @Column(name = "CONTRAPARTE_NUMERO_BOLETA", nullable = false)
+  private Integer contraparteNumeroBoleta;
+
+  @Column(name = "CONTRAPARTE_OPERADOR", nullable = false)
+  private String contraparteOperador;
+
+  @Column(name = "DATA_IDA", nullable = false)
+  private LocalDate dataIda;
+
+  @Column(name = "DATA_VOLTA", nullable = false)
+  private LocalDate dataVolta;
+
+  @Column(name = "TAXA_PRE", nullable = false)
+  private BigDecimal taxaPre;
+
+  @Column(name = "TAXA_EFETIVA", nullable = false)
+  private BigDecimal taxaEfetiva;
+
+  @Column(name = "VALOR_ALVO", nullable = false)
+  private BigDecimal valorAlvo;
+
+  @Column(name = "NUMERO_BOLETA")
+  private String boleta;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "FORMA_MENSURACAO", nullable = false)
+  private FormaMensuracaoEnum formaMensuracao;
+
+  @Column(name = "CUSTOS_VALOR_CORRETAGEM", nullable = false)
+  private BigDecimal custosValorCorretagem;
+
+  @ManyToOne
+  @JoinColumn(name = "CUSTOS_INDICADOR_FINANCEIRO", nullable = false)
+  private IndicadorFinanceiro custosIndicadorFinanceiro;
+
+  @OneToMany(mappedBy = "operacaoRendaFixaCompromissada", cascade = CascadeType.PERSIST, orphanRemoval = true)
+  private List<OperacaoRendaFixaCompromissadaLastro> lastros;
+
+  @Override
+  public void prePersist() {
+    try {
+      lastros.forEach(l -> l.setOperacaoRendaFixaCompromissada(this));
+    } catch (final NullPointerException ignored) {
+    }
+  }
+
 }
--- src/main/java/br/com/poupex/investimento/recursosfinanceiros/domain/entity/OperacaoRendaFixaCompromissadaLastro.java
+++ src/main/java/br/com/poupex/investimento/recursosfinanceiros/domain/entity/OperacaoRendaFixaCompromissadaLastro.java
@@ -1,9 +1,7 @@
 package br.com.poupex.investimento.recursosfinanceiros.domain.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import java.math.BigDecimal;
+import javax.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -18,5 +16,24 @@ public class OperacaoRendaFixaCompromissadaLastro extends AbstractEntidadeBase {
   @ManyToOne
   @JoinColumn(name = "OPERACAO_RENDA_FIXA_COMPROMISSADA")
   private OperacaoRendaFixaCompromissada operacaoRendaFixaCompromissada;
+
+  @ManyToOne
+  @JoinColumn(name = "INSTRUMENTO_FINANCEIRO", nullable = false)
+  private InstrumentoFinanceiro instrumentoFinanceiro;
+
+  @Column(name = "QUANTIDADE", nullable = false)
+  private Long quantidade;
+
+  @Column(name = "PRECO_UNITARIO_IDA", nullable = false)
+  private BigDecimal precoUnidadeIda;
+
+  @Column(name = "PRECO_UNITARIO_VOLTA", nullable = false)
+  private BigDecimal precoUnidadeVolta;
+
+  @Column(name = "VALOR_FINANCEIRO_IDA", nullable = false)
+  private BigDecimal valorFinanceiroIda;
+
+  @Column(name = "VALOR_FINANCEIRO_VOLTA", nullable = false)
+  private BigDecimal valorFinanceiroVolta;
 
 }
