2ce65d77948f04f9a4c91305aa0142ecec2fa8d9
Leonardo Ferreira Alves <leonardo.alves@implantainformatica.com.br>
2023-01-24 16:23:25 -0300

    Ajuste calculo

--- src/main/java/br/com/poupex/investimento/recursosfinanceiros/service/CalculaPrecoUnitarioVoltaService.java
+++ src/main/java/br/com/poupex/investimento/recursosfinanceiros/service/CalculaPrecoUnitarioVoltaService.java
@@ -3,9 +3,12 @@ package br.com.poupex.investimento.recursosfinanceiros.service;
 import br.com.poupex.investimento.recursosfinanceiros.domain.model.CalculoPrecoUnitarioVoltaInput;
 import br.com.poupex.investimento.recursosfinanceiros.domain.model.ResponseModel;
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.time.Period;
 import java.time.temporal.ChronoUnit;
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,10 +19,9 @@ public class CalculaPrecoUnitarioVoltaService {
 
   public ResponseModel execute(final CalculoPrecoUnitarioVoltaInput input) {
     var dias = Math.abs(ChronoUnit.DAYS.between(input.getDataIda(), input.getDataVolta()));
-    return new ResponseModel(calculaTaxaDiariaIndicadorService.execute(input.getTaxaAnual(), BigDecimal.valueOf(dias))
-      .multiply(input.getPrecoUnitarioIda())
-      .add(input.getPrecoUnitarioIda())
-    );
+    val calculo = calculaTaxaDiariaIndicadorService.execute(input.getTaxaAnual(), BigDecimal.valueOf(dias))
+      .multiply(input.getPrecoUnitarioIda(), MathContext.DECIMAL128).add(input.getPrecoUnitarioIda());
+    return new ResponseModel(calculo.setScale(8, RoundingMode.DOWN));
   }
 
 }
--- src/main/java/br/com/poupex/investimento/recursosfinanceiros/service/CalculaTaxaDiariaIndicadorService.java
+++ src/main/java/br/com/poupex/investimento/recursosfinanceiros/service/CalculaTaxaDiariaIndicadorService.java
@@ -3,6 +3,7 @@ package br.com.poupex.investimento.recursosfinanceiros.service;
 import br.com.poupex.investimento.recursosfinanceiros.domain.enums.IndicadorFinanceiroPeriodicidade;
 import java.math.BigDecimal;
 import java.math.MathContext;
+import java.math.RoundingMode;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.springframework.stereotype.Service;
@@ -11,8 +12,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CalculaTaxaDiariaIndicadorService {
 
-  private final static MathContext context = new MathContext(8);
-  private final static BigDecimal ANO = BigDecimal.ONE.divide(BigDecimal.valueOf(252), context);
+  private final static BigDecimal ANO = BigDecimal.ONE.divide(BigDecimal.valueOf(252), MathContext.DECIMAL128);
   private final static BigDecimal CEM = BigDecimal.valueOf(100);
 
   public BigDecimal execute(final IndicadorFinanceiroPeriodicidade periodicidade, final BigDecimal valor) {
@@ -23,9 +23,9 @@ public class CalculaTaxaDiariaIndicadorService {
   }
 
   public BigDecimal execute(final BigDecimal valor, final BigDecimal quantidade) {
-    val fatorial = valor.divide(CEM, context).add(quantidade, context);
+    val fatorial = valor.divide(CEM, MathContext.DECIMAL128).add(quantidade);
     val calculo = BigDecimal.valueOf(Math.pow(fatorial.doubleValue(), ANO.doubleValue()));
-    return calculo.subtract(BigDecimal.ONE, context);
+    return calculo.subtract(BigDecimal.ONE);
   }
 
 }
