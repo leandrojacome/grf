4f271e4abc49027bdb639c4c156a590fc76f2895
EMERSON Esteves Martins <emersonmartins.eireli@poupex.com.br>
2023-02-09 09:52:54 -0300

    GIF-506 mapeamento das tabelas

--- src/main/java/br/com/poupex/investimento/recursosfinanceiros/domain/entity/OperacaoRendaFixaDefinitivaPrimario.java
+++ src/main/java/br/com/poupex/investimento/recursosfinanceiros/domain/entity/OperacaoRendaFixaDefinitivaPrimario.java
@@ -1,8 +1,16 @@
 package br.com.poupex.investimento.recursosfinanceiros.domain.entity;
 
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import br.com.poupex.investimento.recursosfinanceiros.domain.enums.TipoTaxa;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,5 +21,32 @@ import lombok.Setter;
 @Entity
 @Table(name = "OPERACAO_RENDA_FIXA_DEFINITIVA", schema = "GESTAO_RECURSOS_FINANCEIROS")
 public class OperacaoRendaFixaDefinitivaPrimario extends OperacaoRendaFixaDefinitiva {
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name = "TIPO_TAXA", nullable = false)
+	private TipoTaxa tipoTaxa;
+
+	@Column(name = "TAXA_PRE", nullable = true)
+	private BigDecimal taxaPre;
+
+	@Column(name = "DIAS_UTEIS", nullable = false)
+	private Boolean diasUteis;
+
+	@Column(name = "TAXA_EFETIVA", nullable = true)
+	private BigDecimal taxaEfetiva;
+
+	@ManyToOne
+	@JoinColumn(name = "INDICE")
+	private IndicadorFinanceiro indice;
+
+	@ManyToOne
+	@JoinColumn(name = "INDICE_NEGOCIACAO")
+	private IndicadorFinanceiro indiceNegociacao;
+
+	@Column(name = "PU_EMISSAO", nullable = false)
+	private BigDecimal puEmissao;
+
+	@Column(name = "VALOR_RESGATE", nullable = false)
+	private BigDecimal valorResgate;
 
 }
--- src/main/java/br/com/poupex/investimento/recursosfinanceiros/domain/entity/OperacaoRendaFixaDefinitivaSecundario.java
+++ src/main/java/br/com/poupex/investimento/recursosfinanceiros/domain/entity/OperacaoRendaFixaDefinitivaSecundario.java
@@ -1,8 +1,16 @@
 package br.com.poupex.investimento.recursosfinanceiros.domain.entity;
 
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import br.com.poupex.investimento.recursosfinanceiros.domain.enums.TipoTaxa;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,5 +21,74 @@ import lombok.Setter;
 @Entity
 @Table(name = "OPERACAO_RENDA_FIXA_DEFINITIVA", schema = "GESTAO_RECURSOS_FINANCEIROS")
 public class OperacaoRendaFixaDefinitivaSecundario extends OperacaoRendaFixaDefinitiva {
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "TIPO_TAXA", nullable = false)
+	private TipoTaxa tipoTaxa;
+
+	@Column(name = "TAXA_PRE", nullable = true)
+	private BigDecimal taxaPre;
+
+	@Column(name = "DIAS_UTEIS_TAXA_PRE", nullable = false)
+	private Boolean diasUteisTaxaPre;
+
+	@Column(name = "TAXA_EFETIVA", nullable = true)
+	private BigDecimal taxaEfetiva;
+
+	@Column(name = "TAXA_NEGOCIACAO", nullable = true)
+	private BigDecimal taxaNegociacao;
+
+	@Column(name = "TAXA_PRE_NEGOCIACAO", nullable = true)
+	private BigDecimal taxaPreNegociacao;
+
+	@Column(name = "DIAS_UTEIS_TAXA_PRE_NEGOCIACAO", nullable = false)
+	private Boolean diasUteisTaxaPreNegociacao;
+
+	@ManyToOne
+	@JoinColumn(name = "INDICE")
+	private IndicadorFinanceiro indice;
+
+	@Column(name = "PERCENTUAL_INDICE", nullable = true)
+	private BigDecimal percentualIndice;
+
+	@Column(name = "DIAS_UTEIS_INDICE", nullable = false)
+	private Boolean diasUteisIndice;
+
+	@ManyToOne
+	@JoinColumn(name = "INDICE_NEGOCIACAO")
+	private IndicadorFinanceiro indiceNegociacao;
+
+	@Column(name = "PERCENTUAL_NEGOCIACAO", nullable = true)
+	private BigDecimal percentualNegociacao;
+
+	@Column(name = "DIAS_UTEIS_INDICE_NEGOCIACAO", nullable = false)
+	private Boolean diasUteisIndiceNegociacao;
+
+	@Column(name = "PU_AGIO_DESAGIO", nullable = true)
+	private BigDecimal puAgioDesagio;
+
+	@Column(name = "PU_ATUAL", nullable = true)
+	private BigDecimal puAtual;
+
+	@Column(name = "PU_NEGOCIADO", nullable = true)
+	private BigDecimal puNegociado;
+
+	@Column(name = "PU_POUPEX", nullable = true)
+	private BigDecimal puPoupex;
+
+	@Column(name = "PU_CONTRAPARTE", nullable = true)
+	private BigDecimal puContraparte;
+
+	@Column(name = "VALOR_FINANCEIRO_ATUAL", nullable = true)
+	private BigDecimal valorFinanceiroAtual;
+
+	@Column(name = "VALOR_FINANCEIRO_NEGOCIACAO", nullable = true)
+	private BigDecimal valorFinanceiroNegociacao;
+
+	@Column(name = "VALOR_FINANCEIRO_POUPEX", nullable = true)
+	private BigDecimal valorFinanceiroPoupex;
+
+	@Column(name = "VALOR_FINANCEIRO_CONTRAPARTE", nullable = true)
+	private BigDecimal valorFinanceiroContraparte;
 
 }
