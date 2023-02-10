0fd13761795341626c26bc0618d3037b906f9006
Hugo <hugonobrega.eireli@poupex.com.br>
2023-02-10 10:07:14 -0300

    Ajustando client de listar todos os instrumentos financeiros do GIF com filtro.

--- src/main/java/br/com/poupex/investimento/recursosfinanceiros/api/controller/TestesController.java
+++ src/main/java/br/com/poupex/investimento/recursosfinanceiros/api/controller/TestesController.java
@@ -1,39 +1,31 @@
 package br.com.poupex.investimento.recursosfinanceiros.api.controller;
 
+import br.com.poupex.investimento.recursosfinanceiros.domain.model.gif.FilterInstrumentoFinanceiroGifInput;
+import br.com.poupex.investimento.recursosfinanceiros.domain.model.gif.InstrumentoFinanceiroGifInputOutput;
 import br.com.poupex.investimento.recursosfinanceiros.infrastructure.client.GestaoInstrumentosFinanceirosApiClient;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("testes")
 @Profile("local")
 @RequiredArgsConstructor
 public class TestesController {
-  private final GestaoInstrumentosFinanceirosApiClient gestaoInstrumentosFinanceirosApiClient;
+	private final GestaoInstrumentosFinanceirosApiClient gestaoInstrumentosFinanceirosApiClient;
 
-  @GetMapping("instrumentos/tipos")
-  public ResponseEntity<?> instrumentosTipos() {
-    return ResponseEntity.ok(gestaoInstrumentosFinanceirosApiClient.getTipoInstrumentosFinanceiros());
-  }
+	@GetMapping("instrumentos/tipos")
+	public ResponseEntity<?> instrumentosTipos() {
+		return ResponseEntity.ok(gestaoInstrumentosFinanceirosApiClient.getTipoInstrumentosFinanceiros());
+	}
 
-  @GetMapping("instrumentos")
-  public ResponseEntity<?> instrumentos(
-    @RequestParam(required = false) final String nome,
-    @RequestParam(required = false) final String sigla,
-    @RequestParam(required = false) final List<Long> tiposInstrumentoFinanceiro,
-    @RequestParam(required = false) final Long formaMensuracao,
-    final Pageable pageable
-  ) {
-    return ResponseEntity.ok(gestaoInstrumentosFinanceirosApiClient.getInstrumentosFinanceiros(
-      pageable, tiposInstrumentoFinanceiro, nome, sigla, formaMensuracao
-    ));
-  }
+	@PostMapping("instrumentos")
+	public ResponseEntity<Page<InstrumentoFinanceiroGifInputOutput>> instrumentos(
+			@RequestBody FilterInstrumentoFinanceiroGifInput filter, Pageable pageable) {
+		return ResponseEntity.ok(gestaoInstrumentosFinanceirosApiClient.getInstrumentosFinanceiros(filter, pageable));
+	}
 
 }
